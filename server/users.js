ce6877e3b9e6b6827aa365926cdb3ea5f7b15d3e
kshashwat007 <kshashwat007@gmail.com>
2020-06-13 20:28:31 +0400

    fucntions for removing, getting user

--- server/users.js
+++ server/users.js
@@ -17,14 +17,20 @@ const addUsers = (id, name, room) => {
     return { user }
 }
 
-const removeUsers = () => {
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
 
+    if(index !== -1){
+        return users.splice(index, 1)[0];
+    }
 }
 
-const getUser = () => {
-
+const getUser = (id) => {
+    users.find((user) => user.id === id);
 }
 
-const getUsersInRoom = () => {
-
+const getUsersInRoom = (room) => {
+    users.filter((user) => user.room === room);
 }
+
+module.exports = { addUsers, removeUser, getUser, getUsersInRoom }

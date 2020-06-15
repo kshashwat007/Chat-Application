8b5a8fa0173004d4473f0728c37a44630b4d45b9
kshashwat007 <kshashwat007@gmail.com>
2020-06-15 13:23:57 +0400

    server changes for cors errors

--- server/index.js
+++ server/index.js
@@ -10,9 +10,19 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+io.origins("*:*");
 
 app.use(cors());
 app.use(router);
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+})
 
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
